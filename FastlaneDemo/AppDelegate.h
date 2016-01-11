67e8bdf1a4d66f1c7c0620e47c7f4e61905a3827
lulee007 <lulee007@live.com>
2016-01-11 10:10:54 +0800

    first commit

---
+++ FastlaneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastlaneDemo
+//
+//  Created by 卢小辉 on 16/1/11.
+//  Copyright © 2016年 szmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastlaneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastlaneDemo
+//
+//  Created by 卢小辉 on 16/1/11.
+//  Copyright © 2016年 szmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
